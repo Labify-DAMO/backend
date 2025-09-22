62cdd80549b74f6e4f626296f18077ec281268e2
ownue <enu_i@naver.com>
2025-09-22 17:20:32 +0900

    chore: facility 엔티티 재수정
    - 첫 설계 따라 userId -> managerId로 수정

--- src/main/java/com/labify/backend/facility/entity/Facility.java
+++ src/main/java/com/labify/backend/facility/entity/Facility.java
@@ -25,6 +25,6 @@ public class Facility {
     private String address;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "userId")
+    @JoinColumn(name = "managerId")
     private User manager;
 }
