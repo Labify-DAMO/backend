8b0314d7eb3c0a925d0246618ef42563da0a92d9
ownue <enu_i@naver.com>
2025-09-22 15:52:35 +0900

    fix: build 설정과 SecurityConfig 수정
    - .env 파일을 인식할 수 있도록 라이브러리 추가
    - postman 테스트 가능하도록 securityconfig 파일 임시 수정

--- src/main/java/com/labify/backend/config/SecurityConfig.java
+++ src/main/java/com/labify/backend/config/SecurityConfig.java
@@ -2,13 +2,37 @@ package com.labify.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // 접근 규칙 설정
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // CSRF 보호 비활성화
+                .csrf(csrf -> csrf.disable())
+                // 세션을 사용하지 않도록 설정
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // HTTP 요청에 대한 접근 권한 설정
+                .authorizeHttpRequests(auth -> auth
+                        // "/facilities/register" 경로로 오는 요청은 인증 없이 모두 허용
+                        .requestMatchers("/facilities/register").permitAll()
+                        // 그 외 나머지 모든 요청은 인증을 요구
+                        .anyRequest().authenticated());
+
+        return http.build();
     }
 }
