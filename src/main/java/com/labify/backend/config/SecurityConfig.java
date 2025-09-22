ee2540aa951cfde70f36161baa37410862936ec9
ownue <enu_i@naver.com>
2025-09-22 17:56:35 +0900

    chore: SecurityConfig 파일 수정
    - labs 경로로 들어오는 요청도 인증 필요 없게 테스트용으로 열어둠

--- src/main/java/com/labify/backend/config/SecurityConfig.java
+++ src/main/java/com/labify/backend/config/SecurityConfig.java
@@ -28,8 +28,8 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // HTTP 요청에 대한 접근 권한 설정
                 .authorizeHttpRequests(auth -> auth
-                        // "/facilities/register" 경로로 오는 요청은 인증 없이 모두 허용
-                        .requestMatchers("/facilities/*", "/lab/*").permitAll()
+                        // 아래 경로로 오는 요청은 인증 없이 모두 허용
+                        .requestMatchers("/facilities/*", "/labs/*").permitAll()
                         // 그 외 나머지 모든 요청은 인증을 요구
                         .anyRequest().authenticated());
 
