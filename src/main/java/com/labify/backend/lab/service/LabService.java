2bb757c3c2e886856f89e5f70834a9afccd14778
ownue <enu_i@naver.com>
2025-09-28 16:43:31 +0900

    feat: 연구실 정보 수정 api 추가
    - 연구실 정보 중 이름과 위치 또는 둘 중 하나만 수정도 가능한 api를 추가

--- src/main/java/com/labify/backend/lab/service/LabService.java
+++ src/main/java/com/labify/backend/lab/service/LabService.java
@@ -3,8 +3,10 @@ package com.labify.backend.lab.service;
 import com.labify.backend.facility.entity.Facility;
 import com.labify.backend.facility.repository.FacilityRepository;
 import com.labify.backend.lab.dto.LabRequestDto;
+import com.labify.backend.lab.dto.LabUpdateRequestDto;
 import com.labify.backend.lab.entity.Lab;
 import com.labify.backend.lab.repository.LabRepository;
+import com.labify.backend.user.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ public class LabService {
 
     private final LabRepository labRepository;
     private final FacilityRepository facilityRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public Lab registerLab(LabRequestDto dto) {
@@ -28,5 +31,21 @@ public class LabService {
         lab.setFacility(facility);
 
         return labRepository.save(lab);
+    }
+
+    @Transactional
+    public Lab updateLab(Long labId, LabUpdateRequestDto dto) {
+        Lab existingLab = labRepository.findById(labId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 실험실을 찾을 수 없습니다."));
+
+        // DTO에 값이 있는 필드만 선택적으로 업데이트
+        if (dto.getName() != null) {
+            existingLab.setName(dto.getName());
+        }
+        if (dto.getLocation() != null) {
+            existingLab.setLocation(dto.getLocation());
+        }
+
+        return existingLab;
     }
 }
