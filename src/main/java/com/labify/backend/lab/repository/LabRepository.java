38efdd50a53f627a19b918695e2b6ad1440df374
ownue <enu_i@naver.com>
2025-10-13 17:16:13 +0900

    [WIP] feat: 내 실험실 조회 api
    - 로그인한 사용자가 본인이 소속된 실험실을 조회할 수 있습니다. (시설-실험실 관계에 있는 모든 실험실 조회해오는 방식)

--- src/main/java/com/labify/backend/lab/repository/LabRepository.java
+++ src/main/java/com/labify/backend/lab/repository/LabRepository.java
@@ -2,8 +2,18 @@ package com.labify.backend.lab.repository;
 
 import com.labify.backend.lab.entity.Lab;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LabRepository extends JpaRepository<Lab, Long> {
+
+    /**
+     * 특정 사용자가 UserFacilityRelation을 통해 속해있는 Facility의 모든 Lab 목록을 조회하는 쿼리
+     */
+    @Query("SELECT l FROM Lab l JOIN UserFacilityRelation ufr ON l.facility = ufr.facility WHERE ufr.user.userId = :userId")
+    List<Lab> findLabsByUserId(@Param("userId") Long userId);
 }
